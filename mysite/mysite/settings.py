7968ef02d0f727ac4b61e2e0ee6de202f8d0887a
tenthent <kayratsagynbekov@gmail.com>
2021-03-25 15:56:44 +0600

    48_Отправка писем на Gmail

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -139,3 +139,11 @@ MEDIA_URL = '/media/'
 
 
 INTERNAL_IPS = ['127.0.0.1']
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT =  587 #465 # 2525
+EMAIL_HOST_USER = '9pedobear@gmail.com'
+EMAIL_HOST_PASSWORD = 'your password'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
