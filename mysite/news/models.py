cba2b7ed8c3c1c0c62ca7d4315b7fc4ee4704e80
tenthent <kayratsagynbekov@gmail.com>
2021-03-25 16:30:58 +0600

    49_Визуальный редактор CKEditor

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -11,7 +11,7 @@ class News(models.Model):
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT,
                                  verbose_name='Категория')
-    # views = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"pk": self.pk})
